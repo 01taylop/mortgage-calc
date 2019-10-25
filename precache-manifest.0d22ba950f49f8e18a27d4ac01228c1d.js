5f2d0e2b2d51346c49a8024536ba69888391c672
Patrick Taylor <pattaylor@expediagroup.com>
2019-10-25 21:43:55 +0100

    Updates

---
+++ precache-manifest.0d22ba950f49f8e18a27d4ac01228c1d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a7c8a8818023f4c5735",
+    "url": "/static/js/app.57aa2e21.chunk.js"
+  },
+  {
+    "revision": "01748bfc9c51c385bd7e",
+    "url": "/static/js/runtime~app.306130d3.js"
+  },
+  {
+    "revision": "2050d1d990df6f397f71",
+    "url": "/static/js/2.8d4e1e1b.chunk.js"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "020fd499a2d2bc45c4c3b6021e472b43",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "dd88fd04e240bc667a7e9e3682998dd7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c160e95b200ed402402960a24cf0be0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7865850d0d7222c719129430420f2e",
+    "url": "/static/js/app.57aa2e21.chunk.js.gz"
+  },
+  {
+    "revision": "64efebf6b93e84d5d541da88aa26da33",
+    "url": "/static/js/runtime~app.306130d3.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "7569d8720b86455773b86a6f2cacd285",
+    "url": "/static/js/2.8d4e1e1b.chunk.js.gz"
+  }
+];
